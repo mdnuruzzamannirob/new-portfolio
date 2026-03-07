bb5f59bd67564e2432343ce027bc82a7ea33eca1
Md. Nuruzzaman <dev.mdnuruzzaman@gmail.com>
2026-03-07 14:45:30 +0600

    feat: update max-width for layout components to improve consistency

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pb-6">
+      <div className="max-w-275 mx-auto px-4 sm:px-6 lg:px-8 pb-6">
         <p className="text-center text-sm text-muted">
           &copy; {new Date().getFullYear()} . All rights reserved.
         </p>
